0200f0fe4a3e5f0da655bf960252f5d7757fa67f
LaurenceOMFoisy <mail@mfoisy.com>
2024-06-04 16:05:55 -0400

    add gpt4

--- code/ollama_cleaning/ces21/issue_human_vs_ollama.R
+++ code/ollama_cleaning/ces21/issue_human_vs_ollama.R
@@ -27,6 +27,15 @@ data <- data %>%
     TRUE ~ 0
   ))
 
+data <- data %>%
+  mutate(issue_comparison_gpt4 = case_when(
+    is.na(issue_category_human) & is.na(issue_category_gpt4) ~ 1,
+    is.na(issue_category_human) | is.na(issue_category_gpt4) ~ 0,
+    str_to_lower(issue_category_human) == str_to_lower(issue_category_gpt4) ~ 1,
+    TRUE ~ 0
+  ))
+
 table(data$issue_comparison_mistral, useNA = "ifany")
 table(data$issue_comparison_llama3, useNA = "ifany")
 table(data$issue_comparison_phi3, useNA = "ifany")
+table(data$issue_comparison_gpt4, useNA = "ifany")
