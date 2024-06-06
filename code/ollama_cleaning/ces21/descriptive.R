3f13e44e11fbf5b947c03e8f31edf21934e6134e
LaurenceOMFoisy <mail@mfoisy.com>
2024-06-06 14:24:30 -0400

    mod presentation

--- code/ollama_cleaning/ces21/descriptive.R
+++ code/ollama_cleaning/ces21/descriptive.R
@@ -11,13 +11,29 @@ issue_distribution <- data %>%
   summarise(count = n()) %>%
   arrange(desc(count))
 
+# Define new labels for the issue categories
+new_labels <- c(
+  "Economy and Employment" = "Economy and\nEmployment",
+  "Health and Social Services" = "Health and\nSocial Services",
+  "Governments and Governance" = "Governments and\nGovernance",
+  "Environment and Energy" = "Environment and\nEnergy",
+  "Rights, Liberties, Minorities, and Discrimination" = "Rights, Liberties,\nMinorities,\nand Discrimination",
+  "NA" = "NA",
+  "Education" = "Education",
+  "Culture and Nationalism" = "Culture and\nNationalism",
+  "Immigration" = "Immigration",
+  "Law and Crime" = "Law and Crime"
+)
+
+
 # Create the bar graph
 ggplot(issue_distribution, aes(x = reorder(issue_category_human, -count), y = count)) +
   geom_bar(stat = "identity", fill = "#585858") +
   labs(x = "Issue Category",
        y = "Count") +
   clessnize::theme_clean_light() +
-  theme(axis.text.x = element_text(angle = 50, hjust = 1))
+  theme(axis.text.x = element_text(angle = 50, hjust = 1)) +
+  scale_x_discrete(labels = new_labels)
 
 ggsave("pub_beyond_multiple_choices/graphs/issue_distribution.png",
        width = 8, height = 6, dpi = 300)
